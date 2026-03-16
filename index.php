365fe350d8ea7029bc6ec57f016b4d3b840a139f
faustine2811 <faustine.madec@gmail.com>
2026-03-16 15:32:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 
 // Si l'utilisateur est co, on redirige vers page d'accueil
 if (isset($_SESSION['id_utilisateur'])) {
-    header("Location: ../bbbindex.html");
+    header("Location: ../index.html");
     exit;
 } else {
     // Sinonil va vers la page de login
